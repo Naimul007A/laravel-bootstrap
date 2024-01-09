c73e748a3dd724d4b26eea388897eaa14a881918
Naimul007A <mdnaimul2090@gmail.com>
2024-01-10 00:21:41 +0600

    Readme file edit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @if(isset($title))
+        <title>{{ $title }}</title>
+    @endif
+    <!--livewire -->
+    @livewireStyles
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss','resources/css/main.css'])
+</head>
+<body >
+<div>
+
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
+
+@livewireScripts
+@vite(['resources/js/app.js'])
+</body>
+</html>
